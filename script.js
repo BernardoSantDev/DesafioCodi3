0787ddea6257ad939974294bd8c5c71f18316187
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-03-29 23:01:25 -0300

    cria as const

---
+++ script.js
@@ -0,0 +1,9 @@
+ function updateSystemCore(){
+    const alvo = parseFloat(document.getElementById('alvo').value);
+    const carga = parseFloat(document.getElementById('carga').value);
+    const modobarra = document.getElementById('modobarra');
+    const porcento = document.getElementById('porcento');
+    const mensagem = document.getElementById('mensagem');
+
+
+ }
